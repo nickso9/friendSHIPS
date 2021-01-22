02f570645fab3cc6c4a342612c1bdc25abf497db
nicksandoval <sand8val@gmail.com>
2021-01-22 15:31:46 -0800

    deploy test

--- socket.js
+++ socket.js
@@ -3,10 +3,10 @@ let io;
 module.exports = {
   init: httpServer => {
     io = require('socket.io')(httpServer, {
-      // cors: {
-      // origin: "http://localhost:3000",
-      // methods: ["GET", "POST"]
-      // }
+      cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"]
+      }
     });
     return io;
   },
