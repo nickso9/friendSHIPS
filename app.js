b3880a3f109f54da7af00649fb95352f03cb0439
nicksandoval <sand8val@gmail.com>
2021-01-22 15:16:54 -0800

    deploy test

--- app.js
+++ app.js
@@ -4,8 +4,16 @@ const cors = require('cors')
 
 require('dotenv').config();
 const app = express();
+const port = process.env.PORT || 5000
 
-const server = app.listen(8080, () => console.log('server running.'))
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'))
+    app.get('*', (req,res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+    })
+}
+
+const server = app.listen(port, () => console.log('server running.'))
 const io = require('./socket').init(server);
 
 app.use(cors());
