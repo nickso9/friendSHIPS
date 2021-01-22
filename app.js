1ecf4de090f26336726bdadab4f270cbfc70a0c2
nicksandoval <sand8val@gmail.com>
2021-01-22 15:37:12 -0800

    deploy test

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const app = express();
 const port = process.env.PORT || 5000
 
 
-
+app.use((req, res) => res.sendFile(INDEX, { root: __dirname }))
 const server = app.listen(port, () => console.log('server running.'))
 const io = require('./socket').init(server);
 
