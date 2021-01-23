9b6fe748b78f679ec64ff2ffa951fe8976c186ea
nicksandoval <sand8val@gmail.com>
2021-01-22 16:20:04 -0800

    added path

--- app.js
+++ app.js
@@ -7,12 +7,7 @@ require('dotenv').config();
 const app = express();
 const port = process.env.PORT || 5000
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'))
-    app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
-    })
-}
+
 
 const server = app.listen(port, () => console.log('server running.'))
 const io = require('./socket').init(server);
@@ -50,3 +45,9 @@ const userRoute = require('./routes/actions')
 app.use('/users', usersRoute)
 app.use('/user', userRoute)
 
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'))
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+    })
+}
